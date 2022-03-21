4c9a7d2b681f1e7a6c737c395243b6e81d592eec
Bruno Messias <messias.physics@gmail.com>
2022-03-21 12:06:51 -0300

    perf: numba native parallel vs numpy
    Gamma function whith just numba is faster than numpy

--- tests/test_fast_math.py
+++ tests/test_fast_math.py
@@ -1,14 +1,17 @@
 import numpy as np
+import pytest
 from edgeseraser.misc.fast_math import nbbetaln, nbgammaln
 from scipy.special import betaln, gammaln
 
+SIZE_BENCHMARK = 10000000
+
 
 def test_gammaln():
-    z0 = np.arange(0, 100)
+    z0 = np.arange(0, 100).astype("float64")
     s = z0.shape[0]
     assert np.allclose(gammaln(z0), nbgammaln(z0, s))
     for i in range(1, 3):
-        zs = np.linspace(0.001, 100, 10**i)
+        zs = np.linspace(0.001, 100, 10**i).astype("float64")
         s = zs.shape[0]
         arr_scipy = gammaln(zs)
         arr_fast = nbgammaln(zs, s)
@@ -27,3 +30,16 @@ def test_betaln():
     w0 = np.arange(1000, 1100)
     s = z0.shape[0]
     assert np.allclose(betaln(z0, w0), nbbetaln(z0, w0, s))
+
+
+@pytest.mark.benchmark(group="gamma numba")
+def test_gamma_scipy(benchmark):
+    zs = np.linspace(0.001, 100, SIZE_BENCHMARK)
+    benchmark(gammaln, zs)
+
+
+@pytest.mark.benchmark(group="gamma numba")
+def test_gamma_numba(benchmark):
+    zs = np.linspace(0.001, 100, SIZE_BENCHMARK)
+    s = zs.shape[0]
+    benchmark(nbgammaln, zs, s)
