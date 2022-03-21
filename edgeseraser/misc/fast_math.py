3b3bbc45f2d53f091c757f367e41c96590127911
Bruno Messias <messias.physics@gmail.com>
2022-03-21 12:09:16 -0300

    perf: remove slow gamma numpy implementation

--- edgeseraser/misc/fast_math.py
+++ edgeseraser/misc/fast_math.py
@@ -51,54 +51,6 @@ def nbgammaln(z: np.ndarray, n: int) -> np.ndarray:
     return out
 
 
-@njit(fastmath=True, nogil=False, error_model="numpy", parallel=True)
-def nbgammaln_np(z: np.ndarray, n: int) -> np.ndarray:
-    """Compute the log of the gamma function.
-
-    Algorithm extracted from **Numerical Recipes in C**[1] chapter 6.1
-
-
-    [1]: http://www.it.uom.gr/teaching/linearalgebra/NumericalRecipiesInC/
-
-    Args:
-        z: np.array
-    Returns:
-        np.array
-
-    """
-    coefs = np.array(
-        [
-            57.1562356658629235,
-            -59.5979603554754912,
-            14.1360979747417471,
-            -0.491913816097620199,
-            0.339946499848118887e-4,
-            0.465236289270485756e-4,
-            -0.983744753048795646e-4,
-            0.158088703224912494e-3,
-            -0.210264441724104883e-3,
-            0.217439618115212643e-3,
-            -0.164318106536763890e-3,
-            0.844182239838527433e-4,
-            -0.261908384015814087e-4,
-            0.368991826595316234e-5,
-        ]
-    )
-    ones = np.ones(n, dtype=np.float64)
-    y = ones * z
-    tmp = z + 5.24218750000000000
-    tmp = (z + 0.5) * np.log(tmp) - tmp
-    ser = ones * 0.999999999999997092
-
-    for j in prange(14):
-        y = y + 1.0
-        cc = coefs[j]
-        ser = ser + cc / y
-
-    out = tmp + np.log(2.5066282746310005 * ser / z)
-    return out
-
-
 @njit(fastmath=True, error_model="numpy")
 def nbbeta(z, w, n):
     """Compute the beta function.
