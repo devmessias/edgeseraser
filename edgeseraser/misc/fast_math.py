90c814206851b0c754f34751bcaa0e4e297bac65
Bruno Messias <messias.physics@gmail.com>
2022-03-22 10:49:48 -0300

    perf:implement a simple gammaln cache JIT class

--- edgeseraser/misc/fast_math.py
+++ edgeseraser/misc/fast_math.py
@@ -2,10 +2,80 @@
 
 import numpy as np
 from numba import njit, prange
+from numba.experimental import jitclass
+from numba.typed import Dict as nb_Dict
+from numba.types import DictType as nb_DictType
+from numba.types import float64 as nb_float64
+
+
+@jitclass({"lru": nb_DictType(nb_float64, nb_float64)})
+class NbGammaLnCache:
+    def __init__(self):
+        self.lru = nb_Dict.empty(nb_float64, nb_float64)
+
+    def put(self, z, v):
+        self.lru[z] = v
+
+    def callp(self, z):
+        if z not in self.lru:
+            r = nbgammaln(z)
+            self.lru[z] = r
+        return self.lru[z]
+
+    def call(self, z):
+        if z not in self.lru:
+            r = nbgammaln(z)
+            return r
+        return self.lru[z]
+
+
+@njit(fastmath=True, nogil=True, error_model="numpy")
+def nbgammaln(z: float) -> float:
+    """Compute the log of the gamma function.
+
+    Algorithm extracted from **Numerical Recipes in C**[1] chapter 6.1
+
+
+    [1]: http://www.it.uom.gr/teaching/linearalgebra/NumericalRecipiesInC/
+
+    Args:
+        z: np.float64
+    Returns:
+        np.float64
+    """
+    coefs = np.array(
+        [
+            57.1562356658629235,
+            -59.5979603554754912,
+            14.1360979747417471,
+            -0.491913816097620199,
+            0.339946499848118887e-4,
+            0.465236289270485756e-4,
+            -0.983744753048795646e-4,
+            0.158088703224912494e-3,
+            -0.210264441724104883e-3,
+            0.217439618115212643e-3,
+            -0.164318106536763890e-3,
+            0.844182239838527433e-4,
+            -0.261908384015814087e-4,
+            0.368991826595316234e-5,
+        ]
+    )
+    y = z
+    f = 2.5066282746310005 / z
+    tmp = z + 5.24218750000000000
+    tmp = (z + 0.5) * np.log(tmp) - tmp
+    ser = 0.999999999999997092
+    for j in range(14):
+        y = y + 1.0
+        ser = ser + coefs[j] / y
+
+    out = tmp + np.log(f * ser)
+    return out
 
 
 @njit(fastmath=True, nogil=True, error_model="numpy", parallel=True)
-def nbgammaln(z: np.ndarray, n: int) -> np.ndarray:
+def nbgammaln_parallel(z: np.ndarray) -> np.ndarray:
     """Compute the log of the gamma function.
 
     Algorithm extracted from **Numerical Recipes in C**[1] chapter 6.1
@@ -37,6 +107,7 @@ def nbgammaln(z: np.ndarray, n: int) -> np.ndarray:
             0.368991826595316234e-5,
         ]
     )
+    n = z.shape[0]
     out = np.zeros(n)
     for i in prange(n):
         y = z[i]
@@ -52,8 +123,8 @@ def nbgammaln(z: np.ndarray, n: int) -> np.ndarray:
 
 
 @njit(fastmath=True, error_model="numpy")
-def nbbeta(z, w, n):
-    """Compute the beta function.
+def nbbetaln_parallel(z, w):
+    """Compute the  LogBeta function.
 
     Algorithm extracted from **Numerical Recipes in C**[1] chapter 6.1
 
@@ -66,28 +137,9 @@ def nbbeta(z, w, n):
     Returns:
         np.array
 
+    $\\beta = \\Gamma(z) + \\Gamma(w) - \\Gamma(z + w)$
+    $\\ln(\\abs \\beta)$
     """
-    arg = nbgammaln(z, n) + nbgammaln(w, n) - nbgammaln(z + w, n)
-    beta = np.exp(arg)
-    return beta
 
-
-@njit(fastmath=True, error_model="numpy")
-def nbbetaln(z, w, n):
-    """Compute the log of the beta function.
-
-    We know
-    $$ \\ln\\beta = \\ln\\abs(\\beta)$$
-
-
-
-    Args:
-        z: np.array
-    Returns:
-        np.array
-
-    """
-    beta = nbbeta(z, w, n)
-    out = np.log(np.abs(beta))
-
-    return out
+    arg = nbgammaln_parallel(z) + nbgammaln_parallel(w) - nbgammaln_parallel(z + w)
+    return arg
--- tests/test_fast_math.py
+++ tests/test_fast_math.py
@@ -1,63 +1,53 @@
 import numpy as np
 import pytest
-from edgeseraser.misc.fast_math import nbbetaln, nbgammaln
+from edgeseraser.misc.fast_math import (
+    NbGammaLnCache,
+    nbbetaln_parallel,
+    nbgammaln_parallel,
+)
 from scipy.special import betaln, gammaln
-
-SIZE_BENCHMARK = 1000000
 
 
 def test_gammaln():
     z0 = np.arange(0, 100).astype("float64")
-    s = z0.shape[0]
-    assert np.allclose(gammaln(z0), nbgammaln(z0, s))
+    assert np.allclose(gammaln(z0), nbgammaln_parallel(z0))
     for i in range(1, 3):
         zs = np.linspace(0.001, 100, 10**i).astype("float64")
-        s = zs.shape[0]
         arr_scipy = gammaln(zs)
-        arr_fast = nbgammaln(zs, s)
+        arr_fast = nbgammaln_parallel(zs)
         assert np.allclose(arr_scipy, arr_fast)
+
+
+def test_lru_gammaln():
+    z0 = np.arange(0, 100).astype("float64")
+    z0 = np.array([1, 2.0, 4.0])
+    s = z0.shape[0]
+    gamaln_lru = NbGammaLnCache()
+    scipy_r = gammaln(z0)
+    fast_r = np.zeros_like(z0)
+    for i in range(s):
+        fast_r[i] = gamaln_lru.callp(z0[i])
+    assert np.allclose(scipy_r, fast_r)
 
 
 def test_betaln():
     for i in range(1, 3):
         zs = np.linspace(0.001, 100, 10**i)
         ws = np.linspace(0.001, 100, 10**i)
-        s = zs.shape[0]
         arr_scipy = betaln(zs, ws)
-        arr_fast = nbbetaln(zs, ws, s)
+        arr_fast = nbbetaln_parallel(zs, ws)
         assert np.allclose(arr_scipy, arr_fast)
     z0 = np.arange(0, 100)
     w0 = np.arange(1000, 1100)
-    s = z0.shape[0]
-    assert np.allclose(betaln(z0, w0), nbbetaln(z0, w0, s))
+    assert np.allclose(betaln(z0, w0), nbbetaln_parallel(z0, w0))
 
 
+@pytest.mark.parametrize("what", ["nb", "scipy"])
 @pytest.mark.benchmark(group="GammaLn numba vs scipy")
-def test_gammaln_scipy_perf(benchmark):
-    benchmark.extra_info["size array"] = SIZE_BENCHMARK
-    zs = np.linspace(0.001, 100, SIZE_BENCHMARK)
-    benchmark(gammaln, zs)
-
-
-@pytest.mark.benchmark(group="GammaLn numba vs scipy")
-def test_gammaln_numba_perf(benchmark):
-    benchmark.extra_info["size array"] = SIZE_BENCHMARK
-    zs = np.linspace(0.001, 100, SIZE_BENCHMARK)
-    s = zs.shape[0]
-    benchmark(nbgammaln, zs, s)
-
-
-@pytest.mark.benchmark(group="BetaLn numba vs scipy")
-def test_betaln_scipy_perf(benchmark):
-    benchmark.extra_info["size array"] = SIZE_BENCHMARK
-    zs = np.linspace(0.001, 100, SIZE_BENCHMARK)
-    ws = np.linspace(0.001, 100, SIZE_BENCHMARK)
-    benchmark(betaln, zs, ws)
-
-
-@pytest.mark.benchmark(group="BetaLn numba vs scipy")
-def test_betaln_numba_perf(benchmark):
-    benchmark.extra_info["size array"] = SIZE_BENCHMARK
-    zs = np.linspace(0.001, 100, SIZE_BENCHMARK)
-    ws = np.linspace(0.001, 100, SIZE_BENCHMARK)
-    benchmark(nbbetaln, zs, ws, SIZE_BENCHMARK)
+def test_gammaln_nb_vs_scipy_perf(benchmark, what):
+    benchmark.extra_info["size array"] = 1000000
+    zs = np.linspace(0.001, 1000, 1000000)
+    if what == "scipy":
+        benchmark(gammaln, zs)
+    elif what == "nb":
+        benchmark(nbgammaln_parallel, zs)
